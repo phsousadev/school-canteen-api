f0987039670bee9cd8dc5b172a659f0a9954fe68
Pedro Sousa <pedro.sousa@divam.com.br>
2025-05-20 11:08:41 -0300

    chore: configuring global error handler

--- src/http/controllers/register-user.controller.ts
+++ src/http/controllers/register-user.controller.ts
@@ -34,7 +34,7 @@ export async function registerUser(
       return reply.status(409).send({ message: err.message })
     }
 
-    return reply.status(500).send()
+    throw err
   }
 
   return reply.status(201).send()
